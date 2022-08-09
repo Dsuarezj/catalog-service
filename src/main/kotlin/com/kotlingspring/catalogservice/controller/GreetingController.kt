352d9138e0369d3e4f52e324149415367206f500
Dsuarezj <dsuarez@thoughtworks.com>
2022-08-09 14:27:07 +0200

    Inject service using annotations

--- src/main/kotlin/com/kotlingspring/catalogservice/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlingspring/catalogservice/controller/GreetingController.kt
@@ -1,6 +1,7 @@
 package com.kotlingspring.catalogservice.controller
 
 import com.kotlingspring.catalogservice.service.GreetingService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/greetings")
-class GreetingController(val greetingService: GreetingService) {
+class GreetingController() {
+
+    @Autowired
+    val greetingService: GreetingService = GreetingService()
+
     @GetMapping("/{name}")
     fun retrieveGreeting(@PathVariable("name") name: String): String {
         return greetingService.retrieveGreeting(name)
