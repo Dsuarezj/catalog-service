5a62c09ac93cd9554b1438800fa7e6c446de7d9f
Dsuarezj <dsuarez@thoughtworks.com>
2022-08-10 10:41:57 +0200

    Using profiles yml

--- src/main/kotlin/com/kotlingspring/catalogservice/service/GreetingService.kt
+++ src/main/kotlin/com/kotlingspring/catalogservice/service/GreetingService.kt
@@ -1,8 +1,13 @@
 package com.kotlingspring.catalogservice.service
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class GreetingService {
-    fun retrieveGreeting (name: String) = "Hello $name"
+
+    @Value("\${message}")
+    lateinit var message: String
+
+    fun retrieveGreeting (name: String) = "$message $name"
 }
